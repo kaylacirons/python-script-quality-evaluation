6d08761b4cc8c5e37437617e606c3b638cccb84b
Kayla Irons <kaylacirons2@gmail.com>
2025-12-29 13:16:00 -0500

    Create improved_versions.py

---
+++ improved_versions.py
@@ -0,0 +1,18 @@
+"""
+Improved Versions of Evaluated Scripts
+"""
+
+def sum_numbers(nums):
+    return sum(nums)
+
+
+def get_even_numbers(numbers):
+    return [n for n in numbers if n % 2 == 0]
+
+
+def reverse_string(text):
+    return text[::-1]
+
+
+def check_palindrome(word):
+    return word == word[::-1]
